75360cd9a012611890bfa3589c1d8f975faeb7e7
jeffmeng <jkmexpress@yahoo.com>
2016-12-15 00:14:04 +0000

    bug fix

--- js/techedit.js
+++ js/techedit.js
@@ -1,15 +1,16 @@
 /* global firebase $ */
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        $("#nav-login-link").click(function() {
-            window.location.href = "/account/login/?redir=" + encodeURIComponent(window.location.href);
-        });
-        $("#nav-login").removeClass("hidden");
-    }
-    else {
         $("#nav-logout-link").click(function() {
             window.location.href = "/account/logout/?redir=" + encodeURIComponent(window.location.href);
         });
         $("#nav-logout").removeClass("hidden");
+        
+    }
+    else {
+        $("#nav-login-link").click(function() {
+            window.location.href = "/account/login/?redir=" + encodeURIComponent(window.location.href);
+        });
+        $("#nav-login").removeClass("hidden");
     }
 });
