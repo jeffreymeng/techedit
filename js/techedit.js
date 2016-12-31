b5fd7c6a5b4353df116671e6f1af76b914a40aae
jeffmeng <jkmexpress@yahoo.com>
2016-12-31 06:27:04 +0000

    bug fix

--- js/techedit.js
+++ js/techedit.js
@@ -7,16 +7,17 @@ firebase.auth().onAuthStateChanged(function(user) {
             window.location.href = "/account/logout/?redir=" + encodeURIComponent(window.location.href);
         });
         $("#nav-logout").removeClass("hidden");
-        
+
     }
     else {
-        
+
         $("#nav-collection").addClass("hidden");
         $("#nav-logout").addClass("hidden");
         $("#nav-login-link").click(function() {
             if (window.location.pathname !== "/account/logout/" || window.location.pathname !== "/account/logout/index.html" || window.location.pathname !== "/account/login/" || window.location.pathname !== "/account/login/index.html") {
                 window.location.href = "/account/login/?redir=" + encodeURIComponent(window.location.href);
-            } else {
+            }
+            else {
                 window.location.href = "/account/login/";
             }
         });
